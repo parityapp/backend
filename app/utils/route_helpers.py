2952fc4334be5acbf82a7e3e3aec59c1aa64e874
Alastair Paragas <alastairparagas@gmail.com>
2017-07-23 07:24:12 +0200

    Module: Route Helpers

---
+++ app/utils/route_helpers.py
@@ -0,0 +1,19 @@
+from typing import Union
+
+from aiohttp.web import Request
+
+from . import auth
+
+async def get_session_from_request(request: Request) -> Union[
+  auth.UserSession,
+  None
+]:
+  authorization_header = request.headers.get('Authorization')
+  if authorization_header is None:
+    return None
+
+  string_user_session = authorization_header.replace('Bearer ', '')
+  if string_user_session is None:
+    return None
+
+  auth.decode(string_user_session)
