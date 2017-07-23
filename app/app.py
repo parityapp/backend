5baf04175cc61713cd9a5eae14d16b4bec7abf93
Alastair Paragas <alastairparagas@gmail.com>
2017-07-23 08:03:10 +0200

    App Router

---
+++ app/app.py
@@ -0,0 +1,46 @@
+import os
+
+from aiohttp.web import Request, Response, json_response, Application, run_app
+import aiohttp_cors
+
+from . import auth_controller, stats_controller
+
+
+async def index(_: Request) -> Response:
+  return Response(
+    text="Welcome to Parity Backend API",
+    status=200
+  )
+
+app = Application()
+app.router.add_get(path='/',
+                   handler=index)
+app.router.add_post(path='/auth',
+                    handler=auth_controller.auth_route)
+app.router.add_get(path='/stats/pulse',
+                   handler=stats_controller.pulse_route)
+app.router.add_get(path='/stats/pulse/{channel_id}',
+                   handler=stats_controller.pulse_by_channel_route)
+app.router.add_get(path='/stats/most_active/{channel_id}',
+                   handler=stats_controller.most_active_by_channel_route)
+app.router.add_get(path='/stats/hot_topics/{channel_id}',
+                   handler=stats_controller.hot_topics_by_channel_route)
+app.router.add_get(path='/stats/representative_messages/{channel_id}',
+                   handler=stats_controller.representative_messages_by_channel_route)
+app.router.add_get(path='/stats/summary/{channel_id}',
+                   handler=stats_controller.summary_by_channel_route)
+
+cors = aiohttp_cors.setup(app, defaults={
+  "*": aiohttp_cors.ResourceOptions(
+    allow_credentials=True,
+    expose_headers="*",
+    allow_headers="*",
+  )
+})
+for route in list(app.router.routes()):
+    cors.add(route)
+
+run_app(
+  app,
+  port=int(os.getenv('BACKEND_PORT', '80'))
+)
